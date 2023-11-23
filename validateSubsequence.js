e68bb3ee04ef07630fb545ce6b001fe31b6f2253
JuanCeja <jmc347ls2@yahoo.com>
2023-11-22 20:15:44 -0800

    create solution for isValidSubsequence algorithm

--- validateSubsequence.js
+++ validateSubsequence.js
@@ -1,13 +1,14 @@
-s // Given two non-empty arrays of integers, write a function that determines whether the second arrays is a subsequence of the first one. 
+// Given two non-empty arrays of integers, write a function that determines whether the second arrays is a subsequence of the first one. 
 
 // A subsequence of an array is a set of number that arent necessarily adjacent in the array but that are in the same order as they appear in the array. For instance, the numbers [1, 3, 4] form a subsequence of the array [1, 2, 3, 4], and so do the numbers [2, 4]. Note that a single number in an array and the array itself are both valid subsequences of the array.
 
 function isValidSubsequence(array, sequence) {
-    // create pointer
-    // have pointer point at the first index of the sequence array
-    // iterate through the given array
-        // compare current value to the pointer. if match is made, we shift that value off the sequence array
-    // if no values exist return true else return false
+    for (let i = 0; i < array.length; i++) {
+        if (sequence[0] === array[i]) {
+            sequence.shift();
+        }
+    }
+    return sequence.length ? false : true;
 };
 
 console.log(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10])); // true
