fafce74d0948e216bff641db165f15ca86bf8300
JuanCeja <jmc347ls2@yahoo.com>
2023-11-24 08:21:12 -0800

    create pseudo code for sortedSquaredArray

--- arrays/easy/sortedSquaredArray.js
+++ arrays/easy/sortedSquaredArray.js
@@ -5,7 +5,11 @@
 // output will be: [1, 4, 9, 25, 36, 64, 81]
 
 function sortedSquaredArray(array) {
-    
+    // create 2 pointers
+    // create our return array
+    // while loop. left and right until meet
+        // compare left to right. if left is less push left and vise versa, also increment or decrement pointers
+    // return our array
 };
 
 console.log(sortedSquaredArray([1, 2, 3, 5, 6, 8, 9])) // [1, 4, 9, 25, 36, 64, 81]
