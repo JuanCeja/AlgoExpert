61ea8d11538b59cc2d401f2ac5e6df64a2496eba
JuanCeja <jmc347ls2@yahoo.com>
2023-11-25 16:31:17 -0800

    create sorting function for Non-ConstructableChange algorithm

--- arrays/easy/non-constructableChange.js
+++ arrays/easy/non-constructableChange.js
@@ -5,8 +5,40 @@
 // Sample Input:
 // coins = [5, 7, 1, 1, 2, 3, 22] = 20
 
+function mergeSort(array) {
+    if (array.length <= 1) return array;
+    let mid = Math.floor(array.length / 2);
+    let left = mergeSort(array.slice(0, mid));
+    let right = mergeSort(array.slice(mid));
+    return merge(left, right);
+};
+
+function merge(left, right) {
+    let p1 = 0;
+    let p2 = 0;
+    let sortedArray = [];
+    while(p1 < left.length && p2 < right.length) {
+        if(left[p1] < right[p2]) {
+            sortedArray.push(left[p1]);
+            p1++;
+        } else {
+            sortedArray.push(right[p2]);
+            p2++;
+        }
+    };
+    while(p1 < left.length) {
+        sortedArray.push(left[p1]);
+        p1++;
+    };
+    while(p2 < right.length) {
+        sortedArray.push(right[p2]);
+        p2++;
+    };
+    return sortedArray;
+};
+
 function nonConstructibleChange(coins) {
-    
+    sortedCoins = mergeSort(coins);
 };
 
 console.log(nonConstructibleChange([5, 7, 1, 1, 2, 3, 22])); // 20
