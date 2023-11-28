099bc05a821c4a9763bfe0b5be9130dae58bba46
JuanCeja <jmc347ls2@yahoo.com>
2023-11-27 21:40:09 -0800

    initial commit for threeNumberSum algorithm

--- arrays/medium/threeNumberSum.js
+++ arrays/medium/threeNumberSum.js
@@ -7,7 +7,7 @@
 // Sample Output = [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
 
 function threeNumberSum(array, targetSum) {
-
+    
 };
 
 console.log(threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0)) // [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
