47cae7c604eb9f8d77006b8710f2fda53f7b1af1
JuanCeja <jmc347ls2@yahoo.com>
2023-11-28 19:36:03 -0800

    create sorting function for smallestDifference algorithm

--- arrays/medium/smallestDifference.js
+++ arrays/medium/smallestDifference.js
@@ -10,6 +10,38 @@
 
 // Sample Output = [28, 26]
 
+function mergeSort(array) {
+    if (array.length <= 1) return array;
+    let mid = Math.floor(array.length / 2);
+    let left = mergeSort(array.slice(0, mid));
+    let right = mergeSort(array.slice(mid));
+    return merge(left, right);
+};
+
+function merge(arr1, arr2) {
+    let mergedArray = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            mergedArray.push(arr1[i]);
+            i++;
+        } else {
+            mergedArray.push(arr2[j]);
+            j++;
+        }
+    };
+    while (i < arr1.length) {
+        mergedArray.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        mergedArray.push(arr2[j]);
+        j++;
+    }
+    return mergedArray;
+};
+
 function smallestDifference(arrayOne, arrayTwo) {
 
 };
