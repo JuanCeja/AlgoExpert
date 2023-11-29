249740e1d423117b7cbfdc02021c64f5e31249da
JuanCeja <jmc347ls2@yahoo.com>
2023-11-28 18:28:04 -0800

    create sort function for threeNumberSum algorithm

--- arrays/medium/threeNumberSum.js
+++ arrays/medium/threeNumberSum.js
@@ -6,7 +6,40 @@
 // Target = 0
 // Sample Output = [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
 
+function mergeSort(array) {
+    if (array.length <= 1) return array;
+    let mid = Math.floor(array.length / 2);
+    let left = mergeSort(array.slice(0, mid));
+    let right = mergeSort(array.slice(mid));
+    return merge(left, right);
+};
+
+function merge(arr1, arr2) {
+    let i = 0;
+    let j = 0;
+    let mergedArray = [];
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            mergedArray.push(arr1[i]);
+            i++;
+        } else {
+            mergedArray.push(arr2[j]);
+            j++;
+        }
+    };
+    while (i < arr1.length) {
+        mergedArray.push(arr1[i]);
+        i++;
+    };
+    while (j < arr2.length) {
+        mergedArray.push(arr2[j]);
+        j++;
+    };
+    return mergedArray;
+};
+
 function threeNumberSum(array, targetSum) {
+    let sortedArray = mergeSort(array);
     
 };
 
