16eb9cd2dfe67e0b58a373296762f676c63681dd
JuanCeja <jmc347ls2@yahoo.com>
2023-11-29 21:26:21 -0800

    create solution for spiralTraverse algorithm

--- arrays/medium/spiralTraverse.js
+++ arrays/medium/spiralTraverse.js
@@ -12,14 +12,40 @@
 // sample output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
 function spiralTraverse(array) {
-    // create our return array
-    // create all our variables that will be needed
+    let result = [];
+    let startRow = 0;
+    let endRow = array.length - 1;
+    let startCol = 0;
+    let endCol = array[0].length - 1;
 
-    // use a while loop to iterate through our array
-        // push all our values needed to the array
-        // increment and decrement our variables
+    while (startRow <= endRow && startCol <= endCol) {
+        for (let i = startCol; i <= endCol; i++) {
+            result.push(array[startRow][i]);
+        };
 
-    // return our results array
+        for (let i = startRow + 1; i <= endRow; i++) {
+            result.push(array[i][endCol]);
+        };
+
+        for (let i = endCol - 1; i >= startCol; i--) {
+            if (endRow > startRow) result.push(array[endRow][i]);
+        };
+
+        for (let i = endRow - 1; i > startRow; i--) {
+            if (endCol > startCol) result.push(array[i][startCol]);
+        };
+
+        startRow++;
+        endRow--;
+        startCol++;
+        endCol--;
+    };
+    return result;
 };
 
-console.log(spiralTraverse([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]));
+console.log(spiralTraverse([
+    [1, 2, 3, 4],
+    [12, 13, 14, 5],
+    [11, 16, 15, 6],
+    [10, 9, 8, 7]
+])); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
