586311c9512d72051961948e91eb6b1e7b31b026
JuanCeja <jmc347ls2@yahoo.com>
2023-11-29 16:21:22 -0800

    create pseudo code for moveElementToEnd

---
+++ arrays/moveElementToEnd.js
@@ -0,0 +1,17 @@
+// You're given an array of integers and an integer. Write a function that moves all instances of that integer in the array to the end of the array and returns the array.
+
+// The function should perform this in place(i.e., it should mutate the input array) and doesn't need to maintain the order of the other integers.
+
+// Sample Input: array = [2, 1, 2, 2, 2, 3, 4, 2]
+//               toMove = 2
+
+// Sample Output: [1, 3, 4, 2, 2, 2, 2, 2]
+
+function moveElementToEnd(array, toMove) {
+    // create a pointer
+    // iterate the array
+        // if current value at index does not equal target we swap it
+    // return our array
+};
+
+console.log(moveElementToEnd([2, 1, 2, 2, 2, 3, 4, 2], 2)); // [1, 3, 4, 2, 2, 2, 2, 2]
