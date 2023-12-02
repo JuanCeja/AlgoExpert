8b7a62949fa898af157332e167bf7c0388a04b25
JuanCeja <jmc347ls2@yahoo.com>
2023-12-01 20:05:04 -0800

    create pseudo code for longestPeak algorithm

--- arrays/medium/longestPeak.js
+++ arrays/medium/longestPeak.js
@@ -2,16 +2,20 @@
 
 // A peak is defined as adjacent integers in the array that are increasing until they reach a tip (the highest value in the peak), at which point they become strictly decreasing. At least three integers are required to form a peak.
 
-// For example, the integers 1, 4, 10, 2 form a peak, but the integers 4, 0, 10 don't and neither do the integers 1, 2, 2, 0. Similiarly, the integers 1,2, 3 don't form a peak because there aren't any strictly decreasing integers after the 3.
+// For example, the integers 1, 4, 10, 2 form a peak, but the integers 4, 0, 10 don't and neither do the integers 1, 2, 2, 0. Similarly, the integers 1,2, 3 don't form a peak because there aren't any strictly decreasing integers after the 3.
 
-// Sample Imput:
+// Sample Input:
 // array = [1, 2, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
 
 // Sample Output:
 // 6 because 0,  10, 6, 5, -1, -3
 
 const longestPeak = (array) => {
-
+    // maxPeak tracker
+    // iterate through the array
+        // keep a counter 
+        // if the current element is a peak reset counter
+    // return maxPeak
 };
 
 console.log(longestPeak([1, 2, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]));
