0009a48716d3621653a1d4ee1de11f153c345f20
JuanCeja <jmc347ls2@yahoo.com>
2023-12-05 21:49:01 -0800

    create solution for arrayOfProducts algorithm

--- arrays/medium/arrayOfProducts.js
+++ arrays/medium/arrayOfProducts.js
@@ -16,16 +16,27 @@
 // 20 is equal to 5 x 1 x 4
 
 function arrayOfProducts(array) {
-    let result = [];
-    let product = 1;
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length; j++) {
-            if (i === j) continue;
-            product = product * array[j];
-        };
-        result.push(product);
-        product = 1;
+    const n = array.length;
+    const leftArray = new Array(n).fill(1);
+    const rightArray = new Array(n).fill(1);
+    const result = [];
+
+    let leftProduct = 1;
+    for (let i = 1; i < n; i++) {
+        leftProduct = leftProduct * array[i - 1];
+        leftArray[i] = leftProduct;
     };
+
+    let rightProduct = 1;
+    for (let i = n - 2; i >= 0; i--) {
+        rightProduct = rightProduct * array[i + 1];
+        rightArray[i] = rightProduct;
+    };
+
+    for (let i = 0; i < n; i++) {
+        result.push(leftArray[i] * rightArray[i]);
+    };
+
     return result;
 };
 
