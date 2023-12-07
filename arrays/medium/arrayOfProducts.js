b5d8fb44205897cfadca88d48e14943b69316196
JuanCeja <jmc347ls2@yahoo.com>
2023-12-06 18:14:06 -0800

    create pseudo code for arrayOfProducts algorithm

--- arrays/medium/arrayOfProducts.js
+++ arrays/medium/arrayOfProducts.js
@@ -16,7 +16,18 @@
 // 20 is equal to 5 x 1 x 4
 
 function arrayOfProducts(array) {
-    
+    // store array length in var
+    // start by creating our left and right array filled with 1s
+    // product var
+    // result array
+    // loop through our array storing our left products
+        //  add our product to left array
+        // multiply product with our current value
+    // reset product back to 1
+    // loop through our array storing our right products
+        //  add our product to left array
+        // multiply product with our current value
+    // calculate our final product for our return array
 };
 
 console.log(arrayOfProducts([5, 1, 4, 2])); // [8, 40, 10, 20]
