6e3abfe00ab135c65caecf0015a9ea359d533b77
JuanCeja <jmc347ls2@yahoo.com>
2023-12-09 11:35:21 -0800

    create solution with O(1) space complexity for firstDuplicateValue algorithm

--- arrays/medium/firstDuplicateValue.js
+++ arrays/medium/firstDuplicateValue.js
@@ -23,11 +23,19 @@
 // 3 is the first integer that appears more than once. 2 also appears more than once, but the second 2 appears after the second 3.
 
 function firstDuplicateValue(array) {
-    // create our var
+    // create our index var
+    let index;
     // iterate through our array
+    for(let i = 0; i < array.length; i++) {
         // calculate our index. 
+        index = Math.abs(array[i]) - 1;
         // if that value is already negative return the value else make it a negative value
+        if(array[index] < 0) return Math.abs(array[i]);
+        else array[index] *= -1;
+    };
     // return -1 if no duplicates are found
+    return -1;
 };
 
 console.log(firstDuplicateValue([2, 1, 5, 2, 3, 3, 4])); // 2
+console.log(firstDuplicateValue([2, 1, 5, 3, 3, 2, 4])); // 3
