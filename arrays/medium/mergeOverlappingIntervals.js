ba53985356a57e281198e2ddaf962ad50fce852d
JuanCeja <jmc347ls2@yahoo.com>
2023-12-11 15:55:40 -0800

    create pseudo code for mergeOverlappingIntervals algorithm

--- arrays/medium/mergeOverlappingIntervals.js
+++ arrays/medium/mergeOverlappingIntervals.js
@@ -13,7 +13,27 @@
 // [[1, 2], [3, 8], [9, 10]]
 
 const mergeOverlappingIntervals = (array) => {
+    // create our return array
+    // create pointer for return array
 
+
+    // iterate through our array
+        // iterate through our inner array
+        // create prev and after
+
+        // if we are on the first index of the array compare it to the previous array at the last index and also compare it to the following index in its array. see if the previous is -1 and the next is +1
+            // set prev and after
+            // if both values are -1 and +1 push prev value, current, and next
+            // if the previous value is -1 push that value and the current value to the result
+            // if the next value is +1 push that value and the current value to the result
+            
+        // if we are on the second index. we compare it to our previous value within its array and compare it to the first value of the next array
+            // set prev and after
+            // if both values are -1 and +1 push prev value, current, and next
+            // if the previous value is -1 push that value and the current value to the result
+            // if the next value is +1 push that value and the current value to the result
+
+    // return our array
 };
 
 console.log(mergeOverlappingIntervals([[1, 2], [3, 5], [4, 7], [6, 8], [9, 10]])); // [[1, 2], [3, 8], [9, 10]]
