136ffe856ccc07ee7cc9823889052f9ecd453449
JuanCeja <jmc347ls2@yahoo.com>
2023-12-13 19:11:06 -0800

    create pseudo code for missingNumbers algorithm

--- arrays/medium/missingNumbers.js
+++ arrays/medium/missingNumbers.js
@@ -6,10 +6,15 @@
 // nums = [1, 4, 3];
 
 // Sample Output: 
-// [2, 5]-- n is 5, meaning the completed list should be[1, 2, 3, 4, 5]
+// [2, 5]-- n is 5, meaning the completed list should be [1, 2, 3, 4, 5]
 
 const missingNumbers = (nums) => {
-
+    // create a set
+    // array
+    // for loop. adding values to set from nums.length + 2;
+    // iterate through nums array removing values
+    // go through set adding values to an array
+    // return array
 };
 
 console.log(missingNumbers([1, 4, 3])); // [2, 5]
