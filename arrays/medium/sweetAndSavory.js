81d0c11711040ee0126ee48c18455530db324539
JuanCeja <jmc347ls2@yahoo.com>
2023-12-19 12:26:57 -0800

    create pseudo code 2nd attempt for sweetAndSavory algorithm

--- arrays/medium/sweetAndSavory.js
+++ arrays/medium/sweetAndSavory.js
@@ -29,27 +29,45 @@
 // Sample Output # 3
 // [-25, 5] -- this pairing gets the exact combined profile of -20
 
-const sweetAndSavory = (dishes, target) => {
-    let result = [0, 0];
-    dishes.sort((a, b) => a - b);
-    let left = 0;
-    let right = dishes.length - 1;
-    let bestDiff = Infinity;
+// const sweetAndSavory = (dishes, target) => {
+//     let result = [0, 0];
+//     dishes.sort((a, b) => a - b);
+//     let left = 0;
+//     let right = dishes.length - 1;
+//     let bestDiff = Infinity;
 
-    while (dishes[left] < 0 && dishes[right] > 0) {
-        const dishSum = dishes[left] + dishes[right];
-        if (dishSum > target) {
-            right--;
-        } else {
-            const currentDiff = target - dishSum;
-            if (currentDiff <= bestDiff) {
-                bestDiff = currentDiff;
-                result = [dishes[left], dishes[right]];
-            };
-            left++;
-        };
-    };
-    return result;
+//     while (dishes[left] < 0 && dishes[right] > 0) {
+//         const dishSum = dishes[left] + dishes[right];
+//         if (dishSum > target) {
+//             right--;
+//         } else {
+//             const currentDiff = target - dishSum;
+//             if (currentDiff <= bestDiff) {
+//                 bestDiff = currentDiff;
+//                 result = [dishes[left], dishes[right]];
+//             };
+//             left++;
+//         };
+//     };
+//     return result;
+// };
+
+const sweetAndSavory = (dishes, target) => {
+    // sort our array
+    // create our pointers
+    // create our bestDiff
+    // create result
+    // while loop
+        // currentSum = left + right
+        // if currentSum > target
+            // bring down right
+        // else
+            // currentDiff = currentSum - target
+            // if currentDiff < bestDiff
+                // bestDiff = currentDiff
+                // update result
+            // bring up left
+    // return result
 };
 
 console.log(sweetAndSavory([-3, -5, 1, 7], 8)); // [-3, 7]
