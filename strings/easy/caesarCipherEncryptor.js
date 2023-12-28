fc52359581fe81cedec0a7762d8dcaa492b75421
JuanCeja <jmc347ls2@yahoo.com>
2023-12-27 18:01:32 -0800

    initial commit for caesarCipherEncryptor algorithm

---
+++ strings/easy/caesarCipherEncryptor.js
@@ -0,0 +1,16 @@
+// Given a non-empty string of lowercase letters and a non-negative integer representing a key, write a function that returns a new string obtained by shifting every letter in the input string by k positions in the alphabet, where k is the key.
+
+// Note that letter should 'wrap' around the alphabet; in other words, the letter Z shifted by one returns the letter A.
+
+// Sample Input:
+// string = "xyz"
+// key = 2
+
+// Sample Output:
+// 'zab'
+
+const caesarCipherEncryptor = (string, key) => {
+
+};
+
+console.log(caesarCipherEncryptor('xyz', 2)); // zab
