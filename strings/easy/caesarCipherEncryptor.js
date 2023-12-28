12ba0e66363247e7005abc85f43312608c1aee53
JuanCeja <jmc347ls2@yahoo.com>
2023-12-27 18:40:07 -0800

    create solution for caeserCipherEncryptor algorithm

--- strings/easy/caesarCipherEncryptor.js
+++ strings/easy/caesarCipherEncryptor.js
@@ -10,7 +10,12 @@
 // 'zab'
 
 const caesarCipherEncryptor = (string, key) => {
-
+    let strArr = [];
+    for(let i = 0; i < string.length; i++) {
+        let charCode = (string.charCodeAt(i) - 97 + key) % 26;
+        strArr.push(String.fromCharCode(charCode + 97));
+    };
+    return strArr.join('');
 };
 
 console.log(caesarCipherEncryptor('xyz', 2)); // zab
