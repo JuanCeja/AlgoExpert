fdbda2145eb23a150fa576bb32e450c97efd2b38
JuanCeja <jmc347ls2@yahoo.com>
2023-12-27 17:08:21 -0800

    initial commit for isPalindrome algorithm

---
+++ strings/easy/palindromeCheck.js
@@ -0,0 +1,13 @@
+// Write a function that takes in a non-empty string and that returns a boolean representing whether the string is a palindrome.
+
+// A palindrome is defined as a string that's written the same forward and backward. Note that single-character strings are palindromes.
+
+// sample input: abcdcba
+// sample output: true
+
+const isPalindrome = (str) => {
+
+};
+
+console.log(isPalindrome('abcdcba')) // true
+console.log(isPalindrome('racecar')) // true
