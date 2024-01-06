fdb925ef53fa7dde404a25d252c2e2e23ea5dac7
JuanCeja <jmc347ls2@yahoo.com>
2024-01-06 10:09:14 -0800

    create optimized solution for longestPalindromicSubstring algorithm

--- strings/medium/longestPalindromicSubstring.js
+++ strings/medium/longestPalindromicSubstring.js
@@ -12,64 +12,23 @@
 
 
 const longestPalindromicSubstring = (string) => {
-    // answer
-    // iterate string
-        // left and right
-        // while left === right
-            // increase the right
-        // while left === right and is in bounds
-            // if string slice left and right > answer length
-                // answer = to our slice
-            // left -- right++
-    // return answer 
-
+    let answer = string[0];
+    for (let i = 1; i < string.length; i++) {
+        let left = i; let right = i;
+        while (string[i] === string[right + 1]) {
+            right++;
+        }
+        while (string[left] === string[right] && left >= 0 && right < string.length) {
+            let substring = string.slice(left, right + 1);
+            if (substring.length > answer.length) {
+                answer = substring;
+            }
+            left--;
+            right++;
+            
+        }
+    }
+    return answer;
 };
 
 console.log((longestPalindromicSubstring("abaxyzzyxf"))); // "xyzzyx"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let answer = string[0];
-
-// for (let i = 1; i < string.length - 1; i++) {
-//     if (string[i - 1] === string[i + 1] || string[i] === string[i + 1]) {
-//         let left = i; let right = i;
-
-//         while (string[i] === string[right + 1]) {
-//             right++;
-//         }
-
-//         while (string[left] === string[right] && left >= 0 && right < string.length) {
-//             const potentialAnswer = string.slice(left, right + 1);
-//             if (potentialAnswer.length > answer.length) {
-//                 answer = potentialAnswer;
-//             }
-//             left--;
-//             right++;
-//         }
-//     }
-// }
-// return answer;
