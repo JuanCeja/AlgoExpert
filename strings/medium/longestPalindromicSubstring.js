47356728cf746f69cea439f896290694dcbb8aa8
JuanCeja <jmc347ls2@yahoo.com>
2024-01-06 09:20:10 -0800

    re initial for longestPalindromicSubstring algorithm

--- strings/medium/longestPalindromicSubstring.js
+++ strings/medium/longestPalindromicSubstring.js
@@ -10,40 +10,56 @@
 // Sample Output:
 // "xyzzyx"
 
-const isPalindrome = (string) => {
-    let left = 0;
-    let right = string.length - 1;
-    while (left < right) {
-        if (string[left] === string[right]) {
-            left++;
-            right--;
-        } else return false;
-    };
-    return true;
-};
 
 const longestPalindromicSubstring = (string) => {
-    let answer = string[0];
-
-    for (let i = 1; i < string.length - 1; i++) {
-        if (string[i - 1] === string[i + 1] || string[i] === string[i + 1]) {
-            let left = i; let right = i;
-
-            while (string[i] === string[right + 1]) {
-                right++;
-            }
-
-            while (string[left] === string[right] && left >= 0 && right < string.length) {
-                const potentialAnswer = string.slice(left, right + 1);
-                if (potentialAnswer.length > answer.length) {
-                    answer = potentialAnswer;
-                }
-                left--;
-                right++;
-            }
-        }
-    }
-    return answer;
+    
 };
 
 console.log((longestPalindromicSubstring("abaxyzzyxf"))); // "xyzzyx"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let answer = string[0];
+
+// for (let i = 1; i < string.length - 1; i++) {
+//     if (string[i - 1] === string[i + 1] || string[i] === string[i + 1]) {
+//         let left = i; let right = i;
+
+//         while (string[i] === string[right + 1]) {
+//             right++;
+//         }
+
+//         while (string[left] === string[right] && left >= 0 && right < string.length) {
+//             const potentialAnswer = string.slice(left, right + 1);
+//             if (potentialAnswer.length > answer.length) {
+//                 answer = potentialAnswer;
+//             }
+//             left--;
+//             right++;
+//         }
+//     }
+// }
+// return answer;
