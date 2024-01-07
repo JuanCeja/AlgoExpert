740ed2c006e23abeed972a12842d8c45215c93c5
JuanCeja <jmc347ls2@yahoo.com>
2024-01-07 11:57:43 -0800

    complete solution for validIPAddresses algorithm

--- strings/medium/validIPAddresses.js
+++ strings/medium/validIPAddresses.js
@@ -24,26 +24,39 @@
 //     "192.16.8.0",
 // ]
 
-function restoreIPAddresses(s):
-    result = []  // To store the final list of valid IP addresses
-    
-    // Backtracking function to explore all possible combinations of segments
-    function backtrack(startIndex, segments):
-        if segments equals 3:
-            // Check the validity of the remaining segment and add to result if valid
-        else:
-            // Try placing dots at different positions and explore further
-            for i from 1 to 3 and startIndex + i <= s.length:
-                // Extract a segment and proceed with the next level of backtracking
+const validIPAddresses = (string) => {
+    if (string.length < 4) return [];
 
-    // Function to check if a segment is a valid number
-    function isValidSegment(segment):
-        // Implement logic to check if the segment is a valid number
-        // (without leading zeros, within the range 0-255)
-        // Return true if valid, false otherwise
+    let validIPAddresses = [];
 
-    // Start the backtracking process with an e
+    for (let i = 1; i < 4; i++) {
+        const part1 = string.substring(0, i)
+        if (!isValidPart(part1)) continue;
 
+        for (let j = i + 1; j < i + 4 && j < string.length; j++) {
+            const part2 = string.substring(i, j);
+            if (!isValidPart(part2)) continue;
+
+            for (let k = j + 1; k < j + 4 && k < string.length; k++) {
+                const part3 = string.substring(j, k);
+                const part4 = string.substring(k, string.length);
+                if (isValidPart(part3) && isValidPart(part4)) {
+                    validIPAddresses.push(`${part1}.${part2}.${part3}.${part4}`)
+                }
+            }
+        }
+
+    }
+
+    return validIPAddresses;
+};
+
+const isValidPart = (part) => {
+    if (part.length > 3 || part.length < 1) return false;
+    const number = +part;
+    if (number > 255) return false;
+    return part.length === number.toString().length;
+}
 
 console.log(validIPAddresses("1921680"));
 // [
