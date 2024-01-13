84106a2eef4c4dd358722826cab7594d88138d41
JuanCeja <jmc347ls2@yahoo.com>
2024-01-12 21:04:26 -0800

    create pseudo code for optimalFreelancing algorithm

--- greedy-algorithms/easy/optimalFreelancing.js
+++ greedy-algorithms/easy/optimalFreelancing.js
@@ -4,16 +4,26 @@
 
 // Note: There is no requirement to complete all of the jobs. Only one job can be worked on at a time, meaning that in some scenarios it will be impossible to complete them all.
 
-Sample Input: [
-    { "deadline": 1, "payment": 1 },
-    { "deadline": 2, "payment": 1 },
-    { "deadline": 2, "payment": 2 },
-]
+// Sample Input: [
+//     { "deadline": 1, "payment": 1 },
+//     { "deadline": 2, "payment": 1 },
+//     { "deadline": 2, "payment": 2 },
+// ]
 
-Sample Output: 3 // Job 0 would be completed first, followed by job 2. Job 1 is not completed.
+// Sample Output: 3 - Job 0 would be completed first, followed by job 2. Job 1 is not completed.
 
 const optimalFreelancing = (jobs) => {
-    
+    // sort the array by payment
+    jobs.sort((a, b) => a.payment - b.payment);
+
+    // create an array of length 7
+    // iterate our array backwards to get highest profit
+        // check to see if index at the deadline is available
+            // if so add payment there and to our profit var too
+        // else if spot is taken 
+            // iterate left so find an open slot
+                // add payment to array and our var too
+
 }
 
 console.log(optimalFreelancing([
