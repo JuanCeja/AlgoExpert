78b5cbda358af768bf85c425ecab53cb56dd205b
JuanCeja <jmc347ls2@yahoo.com>
2024-01-17 19:30:56 -0800

    create pseudo code for productSum algorithm

--- recursion/easy/productSum.js
+++ recursion/easy/productSum.js
@@ -11,7 +11,11 @@
 // Sample Output: 12 -- calculated as: 5 + 2 + 2 * (7 - 1) + 3 + 2 * (6 + 3 * (-13 + 8) + 4)
 
 const productSum = (array) => {
-
+    // sum
+    // iterate the loop 
+        // if element is array call the function * depth
+        // else add the current element to sum
+    // return the sum
 };
 
 console.log(productSum([5, 2, [7, -1], 3, [6, [-13, 8], 4]])); // 12
