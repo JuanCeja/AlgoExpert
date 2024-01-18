f9a707225dbde7b727503bb2ce322b4e7681e95a
JuanCeja <jmc347ls2@yahoo.com>
2024-01-17 20:14:29 -0800

    initial commit for productSum algorithm

--- recursion/easy/productSum.js
+++ recursion/easy/productSum.js
@@ -11,16 +11,7 @@
 // Sample Output: 12 -- calculated as: 5 + 2 + 2 * (7 - 1) + 3 + 2 * (6 + 3 * (-13 + 8) + 4)
 
 const productSum = (array, depth = 1) => {
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (Array.isArray(array[i])) {
-            sum += productSum(array[i], depth + 1);
-        }
-        else {
-            sum += array[i];
-        }
-    }
-    return sum * depth;
+
 };
 
 console.log(productSum([5, 2, [7, -1], 3, [6, [-13, 8], 4]])); // 12
