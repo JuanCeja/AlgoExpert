5f1bf564912b1cd38e360567ea98ff5ff1a4eabf
JuanCeja <jmc347ls2@yahoo.com>
2024-01-17 17:36:59 -0800

    create a more optimized solution for nthFibonacci algorithm

--- recursion/easy/nthFibonacci.js
+++ recursion/easy/nthFibonacci.js
@@ -9,10 +9,15 @@
 // Sample Output #2: 5 // 0, 1, 1, 2, 3, 5
 
 const getNthFib = (n) => {
+    let memo = {};
+
+    if (memo[n]) return memo(n);
+
     if (n === 1) return 0;
     if (n === 2) return 1;
 
-    return getNthFib(n - 2) + getNthFib(n - 1);
+    memo[n] = getNthFib(n - 2) + getNthFib(n - 1);
+    return memo[n];
 };
 
 console.log(getNthFib(5)); // 3
