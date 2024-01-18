eff878e67d9238d8e3d5185e0273677ecd91b831
JuanCeja <jmc347ls2@yahoo.com>
2024-01-17 20:13:39 -0800

    create solution for productSum algorithm

--- recursion/easy/productSum.js
+++ recursion/easy/productSum.js
@@ -10,12 +10,17 @@
 // Sample Input: [5, 2, [7, -1], 3, [6, [-13, 8], 4]]
 // Sample Output: 12 -- calculated as: 5 + 2 + 2 * (7 - 1) + 3 + 2 * (6 + 3 * (-13 + 8) + 4)
 
-const productSum = (array) => {
-    // sum
-    // iterate the loop 
-        // if element is array call the function * depth
-        // else add the current element to sum
-    // return the sum
+const productSum = (array, depth = 1) => {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            sum += productSum(array[i], depth + 1);
+        }
+        else {
+            sum += array[i];
+        }
+    }
+    return sum * depth;
 };
 
 console.log(productSum([5, 2, [7, -1], 3, [6, [-13, 8], 4]])); // 12
