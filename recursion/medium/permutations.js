392695f37057681925be118dcd8d25df05b16e87
JuanCeja <jmc347ls2@yahoo.com>
2024-01-17 21:12:10 -0800

    create pseudo code for getPermutations algorithm

--- recursion/medium/permutations.js
+++ recursion/medium/permutations.js
@@ -6,7 +6,16 @@
 // Sample Output: [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
 
 const getPermutations = (array) => {
+    // if arr lenght and perm length !== 0
+        // push perm to perms
+    // else
+        // loop array
+            // init newArray from current array except value at index i 
+            // init current permutation wihch is previous permutation with adding current value at index
+            // call recursion with newArray, newPerm, perms
 
+    // return perms
+    
 };
 
 console.log(getPermutations([1, 2, 3]));
