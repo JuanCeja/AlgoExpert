2039248ad86ee8bc56616a92eb581ec67b2a3f2b
JuanCeja <jmc347ls2@yahoo.com>
2024-01-18 19:25:44 -0800

    create iterative solution for powerset algorithm

--- recursion/medium/powerset.js
+++ recursion/medium/powerset.js
@@ -8,7 +8,15 @@
 // Sample Output: [[], [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]
 
 const powerset = (array) => {
-
+    const subsets = [[]];
+    for (const ele of array) {
+        const length = subsets.length;
+        for (let i = 0; i < length; i++) {
+            const currentSubset = subsets[i];
+            subsets.push(currentSubset.concat(ele));
+        }
+    }
+    return subsets;
 };
 
 console.log(powerset([1, 2, 3]));
