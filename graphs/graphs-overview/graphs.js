28150fa9a4d7d320fa7391fa02687035bddae4dc
JuanCeja <jmc347ls2@yahoo.com>
2024-02-07 19:42:36 -0800

    improve solution for addEdge method

--- graphs/graphs-overview/graphs.js
+++ graphs/graphs-overview/graphs.js
@@ -8,23 +8,16 @@ class Graph {
     };
 
     addEdge(vertex1, vertex2) {
-        if (!this.adjacencyList[vertex1] && !this.adjacencyList[vertex2]) {
-            this.adjacencyList[vertex1] = [vertex2];
-            this.adjacencyList[vertex2] = [vertex1];
-        } else if (!this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
-            this.adjacencyList[vertex1] = [vertex2];
-        } else if (!this.adjacencyList[vertex2] && this.adjacencyList[vertex1]) {
-            this.adjacencyList[vertex2] = [vertex1];
-        } else {
-            this.adjacencyList[vertex1].push(vertex2);
-            this.adjacencyList[vertex2].push(vertex1);
-        }
-    };
-
-    
+        if(!this.adjacencyList[vertex1]) this.addVertex(vertex1);
+        if(!this.adjacencyList[vertex2]) this.addVertex(vertex2);
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
 };
 
 let g = new Graph();
 g.addVertex('Tokyo');
 g.addVertex('San Francisco');
+g.addEdge('San Francisco', 'Tokyo');
+g.addEdge('New York', 'Seattle');
 console.log(g);
