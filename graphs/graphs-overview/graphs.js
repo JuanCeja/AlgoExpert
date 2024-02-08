863d28bb27e0c23a5795ae87ba12ff59b29b1d48
JuanCeja <jmc347ls2@yahoo.com>
2024-02-07 19:47:19 -0800

    create removeEdge method

--- graphs/graphs-overview/graphs.js
+++ graphs/graphs-overview/graphs.js
@@ -7,17 +7,22 @@ class Graph {
         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     };
 
-    addEdge(vertex1, vertex2) {
-        if(!this.adjacencyList[vertex1]) this.addVertex(vertex1);
-        if(!this.adjacencyList[vertex2]) this.addVertex(vertex2);
-        this.adjacencyList[vertex1].push(vertex2);
-        this.adjacencyList[vertex2].push(vertex1);
+    addEdge(v1, v2) {
+        if(!this.adjacencyList[v1]) this.addVertex(v1);
+        if(!this.adjacencyList[v2]) this.addVertex(v2);
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
     }
+
+    removeEdge(v1, v2) {
+        
+    };
 };
 
 let g = new Graph();
 g.addVertex('Tokyo');
 g.addVertex('San Francisco');
+g.addVertex('Lehi');
 g.addEdge('San Francisco', 'Tokyo');
 g.addEdge('New York', 'Seattle');
 console.log(g);
