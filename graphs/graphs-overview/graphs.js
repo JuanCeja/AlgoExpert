0fb67c0bd8e041b328ab98c1e85045a78cc3139d
JuanCeja <jmc347ls2@yahoo.com>
2024-02-07 19:35:49 -0800

    create addEdge method

--- graphs/graphs-overview/graphs.js
+++ graphs/graphs-overview/graphs.js
@@ -6,4 +6,25 @@ class Graph {
     addVertex(vertex) {
         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     };
-}
+
+    addEdge(vertex1, vertex2) {
+        if (!this.adjacencyList[vertex1] && !this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1] = [vertex2];
+            this.adjacencyList[vertex2] = [vertex1];
+        } else if (!this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1] = [vertex2];
+        } else if (!this.adjacencyList[vertex2] && this.adjacencyList[vertex1]) {
+            this.adjacencyList[vertex2] = [vertex1];
+        } else {
+            this.adjacencyList[vertex1].push(vertex2);
+            this.adjacencyList[vertex2].push(vertex1);
+        }
+    };
+
+    
+};
+
+let g = new Graph();
+g.addVertex('Tokyo');
+g.addVertex('San Francisco');
+console.log(g);
