716a55470fa8bc1bd29d2c6fca368d6fa03fd215
JuanCeja <jmc347ls2@yahoo.com>
2024-02-08 18:59:34 -0800

    create a more implisit pseudo code for DFS recursive graph traversal

--- graphs/graphs-overview/DFS-graphTraversal.js
+++ graphs/graphs-overview/DFS-graphTraversal.js
@@ -25,13 +25,13 @@ class Graph {
     }
 
     dfsRecursive(vertex) {
-        // if vertex is empty
-            // return- this is the base case
-        // add vertex to results list
-        // mark vertex as visited
-        // for each neighbor in vertex's neighbors
-            // if neighbor is not visited
-                // recursively call DFS on neighbor
+        // create a list to store the end result, to be returned at the very end
+        // create an object to store visited vertices
+        // create a helper function which accepts the vertex
+            // the helper function should return early if the vertex is empty
+            // the helper function should place the vertex it accepts into the visited object and push that vertex into the result array
+            // loop over all of the values in the adjacencyList for that vertex
+            // if any of those values have not been visited, recursively invoke the helper function with that vertex
     }
 };
 
