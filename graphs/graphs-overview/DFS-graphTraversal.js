a44bc04f7992adeacc4f1ab60bade3fa103fd833
JuanCeja <jmc347ls2@yahoo.com>
2024-02-13 20:11:04 -0800

    initial commit for addEdge method

--- graphs/graphs-overview/DFS-graphTraversal.js
+++ graphs/graphs-overview/DFS-graphTraversal.js
@@ -6,6 +6,10 @@ class Graph {
     addVertex(vertex) {
         if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
+
+    addEdge(v1, v2) {
+        
+    }
 }
 
 let g = new Graph();
