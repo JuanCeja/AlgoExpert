e3cf7e72ce13df002333f9bc05e4b0d493e66c09
JuanCeja <jmc347ls2@yahoo.com>
2024-02-13 20:15:19 -0800

    create solution for addEdge method

--- graphs/graphs-overview/DFS-graphTraversal.js
+++ graphs/graphs-overview/DFS-graphTraversal.js
@@ -4,11 +4,14 @@ class Graph {
     }
 
     addVertex(vertex) {
-        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
 
     addEdge(v1, v2) {
-        
+        if (!this.adjacencyList[v1]) this.addVertex(v1);
+        if (!this.adjacencyList[v2]) this.addVertex(v2);
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
     }
 }
 
