9c66d3ea9b17b5b0bebdd351e440cbf420c7fc1c
JuanCeja <jmc347ls2@yahoo.com>
2024-02-13 20:30:28 -0800

    create solution for removeVertex method

--- graphs/graphs-overview/DFS-graphTraversal.js
+++ graphs/graphs-overview/DFS-graphTraversal.js
@@ -43,56 +43,6 @@ console.log(g);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Graph {
-//     constructor() {
-//         this.adjacencyList = {};
-//     }
-
-//     addVertex(vertex) {
-//         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-//     };
-
-//     addEdge(v1, v2) {
-//         if (!this.adjacencyList[v1]) this.addVertex(v1);
-//         if (!this.adjacencyList[v2]) this.addVertex(v2);
-//         this.adjacencyList[v1].push(v2);
-//         this.adjacencyList[v2].push(v1);
-//     }
-
-//     removeEdge(v1, v2) {
-//         this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
-//         this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1);
-//     };
-
-//     removeVertex(vertex) {
-//         this.adjacencyList[vertex].forEach(v => this.removeEdge(v, vertex));
-//         delete this.adjacencyList[vertex];
-//     }
-
 //     dfsRecursive(vertex) {
 //         // create a list to store the end result, to be returned at the very end
 //         // create an object to store visited vertices
@@ -102,4 +52,3 @@ console.log(g);
 //             // loop over all of the values in the adjacencyList for that vertex
 //             // if any of those values have not been visited, recursively invoke the helper function with that vertex
 //     }
-// };
