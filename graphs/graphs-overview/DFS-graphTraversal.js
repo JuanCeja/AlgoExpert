99e99de5d0098110537a32fabbeda8df6e7ae321
JuanCeja <jmc347ls2@yahoo.com>
2024-02-13 20:24:32 -0800

    initial commit for removeVertex method

--- graphs/graphs-overview/DFS-graphTraversal.js
+++ graphs/graphs-overview/DFS-graphTraversal.js
@@ -18,6 +18,10 @@ class Graph {
         this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
         this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1);
     }
+
+    removeVertex(vertex) {
+        
+    }
 }
 
 let g = new Graph();
