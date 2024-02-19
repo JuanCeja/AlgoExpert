12453ee847f0724608d8f6a3df71d3498e5d2220
JuanCeja <jmc347ls2@yahoo.com>
2024-02-19 15:13:24 -0800

    create pseudo code for bubble up binary heap method

--- binary-heaps/PriorityQueue/binaryHeaps.js
+++ binary-heaps/PriorityQueue/binaryHeaps.js
@@ -7,4 +7,12 @@ class Heap {
         // push the value into the values property on the heap
         // bubble the value up to its correct spot
     }
+
+    bubbleUp(value) {
+        // create a var called index which is the length of the values property - 1
+        // create a var called parentIndex which is the floor of index-1 / 2
+        // keep looping as long as the values element at the parentIndex is less than the values element at the child index
+            // swap the value of the values element at  the parentIndex with the value of the element property at the child index
+            // set the index to be the parentIndex, and start over
+    }
 }
