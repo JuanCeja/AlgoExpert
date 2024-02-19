5fc989be6620ddb662c0636d3fdec7811d912eaa
JuanCeja <jmc347ls2@yahoo.com>
2024-02-19 15:47:16 -0800

    create solution for bubble up binary heap method

--- binary-heaps/PriorityQueue/binaryHeaps.js
+++ binary-heaps/PriorityQueue/binaryHeaps.js
@@ -4,19 +4,24 @@ class Heap {
     }
 
     insert(value) {
-        // push the value into the values property on the heap
         this.values.push(value);
         let parentIndex = Math.floor(((this.values.length - 1) - 1) / 2);
-        
-        // bubble the value up to its correct spot
-        if(value > this.values[parentIndex]) this.bubbleUp(value);
+
+        if (value > this.values[parentIndex]) this.bubbleUp(value);
     }
 
     bubbleUp(value) {
         // create a var called index which is the length of the values property - 1
+        let index = this.values.length - 1;
         // create a var called parentIndex which is the floor of index-1 / 2
+        let parentIndex = Math.floor((index - 1) / 2);
         // keep looping as long as the values element at the parentIndex is less than the values element at the child index
-            // swap the value of the values element at  the parentIndex with the value of the element property at the child index
+        while (this.values[parentIndex] < this.values[index]) {
+            // swap the value of the values element at the parentIndex with the value of the element property at the child index
+            [this.values[parentIndex], this.values[index]][this.values[index], this.values[parentIndex]];
             // set the index to be the parentIndex, and start over
+            index = parentIndex;
+        }
+
     }
 }
