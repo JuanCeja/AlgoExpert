851204aa3c80ce78accb4b3b4c7e70a3d840bb3d
JuanCeja <jmc347ls2@yahoo.com>
2024-02-20 18:24:56 -0800

    create solution for bubbleDown method for MaxBinaryHeap

--- binary-heaps/PriorityQueue/binaryHeaps.js
+++ binary-heaps/PriorityQueue/binaryHeaps.js
@@ -21,47 +21,55 @@ class MaxBinaryHeap {
     }
 
     extractMax() {
-        let start = 0;
-        let end = this.values.length - 1;
-        [this.values[start], this.values[end]] = [this.values[end], this.values[start]];
-        let oldRoot = this.values.pop();
-        this.bubbleDown();
-        return oldRoot;
+        const max = this.values[0];
+        const end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.bubbleDown();
+        }
+        return max;
     }
 
     bubbleDown() {
         let idx = 0;
-        let length = this.values.length;
-        let element = this.values[0];
+        const length = this.values.length;
+        const element = this.values[0];
 
         while (true) {
             let leftChildIdx = (idx * 2) + 1;
             let rightChildIdx = (idx * 2) + 2;
-            let leftChild;
-            let rightChild;
+            let leftChild, rightChild;
             let swap = null;
 
-            if(leftChildIdx < length) {
+            if (leftChildIdx < length) {
                 leftChild = this.values[leftChildIdx];
-                if(leftChild > element) {
+                if (leftChild > element) {
                     swap = leftChildIdx;
                 }
             }
 
-            if(rightChildIdx < length) {
+            if (rightChildIdx < length) {
                 rightChild = this.values[rightChildIdx];
-                if (rightChild > leftChild) {
+                if (
+                    swap === null && rightChild > element
+                    || swap !== null && rightChild > leftChild
+                ) {
                     swap = rightChildIdx;
                 }
             }
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
         }
-        return oldRoot;
     }
 }
 
 let heap = new MaxBinaryHeap();
-console.log(heap.insert(55));
-console.log(heap.insert(1));
-console.log(heap.insert(45));
-console.log(heap.insert(199));
+heap.insert(55);
+heap.insert(1);
+heap.insert(45);
+heap.insert(199);
+console.log(heap);
 console.log(heap.extractMax());
+console.log(heap);
