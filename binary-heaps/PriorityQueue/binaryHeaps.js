21407e1c9c52738639d949c9c92b712f1f1d36b4
JuanCeja <jmc347ls2@yahoo.com>
2024-02-20 19:20:26 -0800

    create solution for bubbleDown and extractMax method for MaxBinaryHeap

--- binary-heaps/PriorityQueue/binaryHeaps.js
+++ binary-heaps/PriorityQueue/binaryHeaps.js
@@ -21,40 +21,48 @@ class MaxBinaryHeap {
     }
 
     extractMax() {
-        // store max value
         const max = this.values[0];
-        // remove last value
         const end = this.values.pop();
-        // if theres 1 or more values in the values array
         if (this.values.length > 0) {
-            // put our small value at the top and walk it down
             this.values[0] = end;
             this.bubbleDown();
         }
         return max;
+
     }
 
     bubbleDown() {
-        // pointer to our value that well be moving down
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
 
-        // store length and the element for easier code writing
-
-        // while loop
         while (true) {
-            // find our left and right child indexs
+            let leftChildIdx = (idx * 2) + 1;
+            let rightChildIdx = (idx * 2) + 2;
 
-            // our leftChild and rightChild values
-            // a swap var
-            
+            let leftChild, rightChild;
+            let swap = null;
 
-            // if leftChildIdx is in bounds
-            // make our swap
-            
 
-            // same with the right but also make sure to check if both are bigger
-            
-            // if we never swap
-            // make our swap
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild > element)
+                    swap = leftChildIdx;
+            }
+
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    swap === null && rightChild > element
+                    || swap !== null && rightChild > leftChild
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
         }
     }
 }
@@ -65,5 +73,5 @@ heap.insert(1);
 heap.insert(45);
 heap.insert(199);
 console.log(heap);
-console.log(heap.extractMax());
+console.log(heap.extractMax()); // 55, 45, 41, 39, 27, 12, 33, 1, 18
 console.log(heap);
