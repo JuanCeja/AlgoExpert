19d2e5df1e0402d8869211e9bdd378f16fb66aac
JuanCeja <jmc347ls2@yahoo.com>
2024-02-21 17:39:37 -0800

    create solution for extract max method for maxBinaryHeap

--- binary-heaps/PriorityQueue/binaryHeaps.js
+++ binary-heaps/PriorityQueue/binaryHeaps.js
@@ -24,16 +24,43 @@ class MaxBinaryHeap {
         const length = this.values.length;
         [this.values[length - 1], this.values[0]] = [this.values[0], this.values[length - 1]];
         const end = this.values.pop();
-        this.bubbleDown;
+        this.bubbleDown();
         return end;
     }
 
     bubbleDown() {
         let idx = 0;
-        let swap = null;
         const element = this.values[0];
         const length = this.values.length;
+        
+        while (true) {
+            let swap = null;
+            let leftChildIdx = (idx * 2) + 1;
+            let rightChildIdx = (idx * 2) + 2;
+            let leftChild, rightChild;
 
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild > element) {
+                    swap = leftChildIdx;
+                }
+            }
+
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    (swap === null && rightChild > element)
+                    || (swap !== null && rightChild > leftChild)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+
+            if(swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
     }
 }
 
